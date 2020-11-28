b3a2affddeb2df87b859510aea2d2c8fd86005d9
Anishjindal08@gmail.com <anishjindal08@gmail.com>
2020-11-28 09:08:08 -0600

    Simple Testing

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 use core::panic::PanicInfo;
 
 mod vga_buffer;
 
 
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
    println!("Hello World{}", "!");
+
+    #[cfg(test)]
+     test_main();
 
     loop {}
 }
@@ -16,4 +23,20 @@ pub extern "C" fn _start() -> ! {
 fn panic(_info: &PanicInfo) -> ! {
     println!("{}", _info);
     loop {}
+}
+
+//Testing
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
