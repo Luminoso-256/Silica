1602c2b3640ef3a06149ef6e4d7686a1c2d40bcb
Anishjindal08@gmail.com <anishjindal08@gmail.com>
2020-11-28 08:29:23 -0600

    VGA buffer

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,18 @@
 #![no_main]
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+   println!("Hello World{}", "!");
+
     loop {}
 }
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
     loop {}
 }
