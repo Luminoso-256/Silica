eac6fbdc5b5020c4b7f0b98c86031beec12937f5
Anishjindal08@gmail.com <anishjindal08@gmail.com>
2020-11-28 14:32:27 -0600

    Misc updates.

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,23 @@
 use core::panic::PanicInfo;
 
 mod vga_buffer;
-
+mod serial;
 
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    //Actual OS
    println!("Hello World{}", "!");
 
+
+
+
+
+
+
+    //Tests
     #[cfg(test)]
      test_main();
-
     loop {}
 }
 
@@ -32,6 +39,7 @@ fn test_runner(tests: &[&dyn Fn()]) {
     for test in tests {
         test();
     }
+    exit_qemu(QemuExitCode::Success);
 }
 
 #[test_case]
@@ -39,4 +47,21 @@ fn trivial_assertion() {
     print!("trivial assertion... ");
     assert_eq!(1, 1);
     println!("[ok]");
+}
+
+//QEMU exit port
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
